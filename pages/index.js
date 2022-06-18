af792fefd2d1850e77949e2e7f4aeced73073209
Omar Ashraf <omar.ashraf.zeinhom@Hotmail.com>
2022-06-18 19:18:40 +0200

    added-map-box-with-useeffect

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,42 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { useEffect } from "react";
 
 import tw from "tailwind-styled-components";
-import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import mapboxgl from '!mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 
+ 
 mapboxgl.accessToken = 'pk.eyJ1Ijoib21hcmFzaHplaW5ob20iLCJhIjoiY2w0YndzY2pjMWF1bjNjcnlqbDI1OW56YSJ9.kfQVlZWIi3vTQoPvBB8SUw';
 
 
 
+;
+
+
+
 export default function Home() {
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [26.8206, 30.8025],
+      zoom: 3,
+    })
+  
+ 
+    });
+ /**
+  *  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+    });
+  */
   return (
     <Wrapper>
-      <Map>Map</Map>
+      <Map id="map">Map</Map>
       <ActionItems>Start</ActionItems>
     </Wrapper>
   );
