d758fa8a463db8e7a869db821cbeae160bf63dc4
Omar Ashraf <omar.ashraf.zeinhom@Hotmail.com>
2022-06-20 01:33:03 +0200

    added-use-effect-and-fetch-function

--- components/Map/MapComp.js
+++ components/Map/MapComp.js
@@ -6,17 +6,6 @@ mapboxgl.accessToken =
   "pk.eyJ1Ijoib21hcmFzaHplaW5ob20iLCJhIjoiY2w0YndzY2pjMWF1bjNjcnlqbDI1OW56YSJ9.kfQVlZWIi3vTQoPvBB8SUw";
 
 const MapComp = () => {
-  //display map with use effect
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: "map",
-      style:
-        "mapbox://styles/omarashzeinhom/cl4k5qsqz000z14s7wqbpzlqv?optimize=true",
-      center: [26.8206, 30.8025],
-      zoom: 3,
-    });
-  });
-
 
   const addToMap = (map) => {
     // Set marker options.
@@ -27,6 +16,21 @@ const MapComp = () => {
       .setLngLat([26.8206, 30.8025])
       .addTo(map);
 }
+
+
+  //display map with use effect
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style:
+        "mapbox://styles/omarashzeinhom/cl4k5qsqz000z14s7wqbpzlqv?optimize=true",
+      center: [26.8206, 30.8025],
+      zoom: 3,
+    });
+    addToMap(map)
+
+  });
+
 
 
 
