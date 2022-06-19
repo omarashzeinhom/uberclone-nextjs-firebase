f597743e072701184e2e3bbd5445ec2578294bea
Omar Ashraf <omar.ashraf.zeinhom@Hotmail.com>
2022-06-19 12:25:42 +0200

    added-full-gui-for-search-page

--- pages/search.js
+++ pages/search.js
@@ -14,9 +14,19 @@ const search = () => {
       </ButtonContainer>
       {/**Input Container */}
       <InputContainer>
-      <FromToIcons></FromToIcons>
+      <FromToIcons>
+<Circle src="https://res.cloudinary.com/dxgqvvg0z/image/upload/v1655632851/FIXITAPP/nextjs-app-images/search/checkbox-blank-circle-line_uauwe2.svg"/>
+<Line src="https://res.cloudinary.com/dxgqvvg0z/image/upload/v1655633562/FIXITAPP/nextjs-app-images/search/1539792897_qyla5f.svg"/>
+<Circle src="https://res.cloudinary.com/dxgqvvg0z/image/upload/v1655632849/FIXITAPP/nextjs-app-images/search/checkbox-blank-circle-line_1_xub199.svg"/>
+
+
+      </FromToIcons>
+      <InputBoxes>
+      
+      <InputButton>Where to?</InputButton>
         <InputButton>Where to?</InputButton>
-        <InputButton>Where to?</InputButton>
+      </InputBoxes>
+      
       </InputContainer>
       {/**Saved Places */}
       {/**Confirm Location */}
@@ -27,22 +37,30 @@ const search = () => {
 export default search;
 
 const Wrapper = tw.div`
- bg-gray-100 h-screen 
+ bg-gray-500 h-screen 
 `;
 
 const ButtonContainer = tw.div`
-bg-white px-4
+bg-gray-100 px-4
 `;
 
 const BackButton = tw.img`
  w-12 h-12 rounded-full`;
 
 const InputContainer = tw.div`
+bg-white
 
 `;
 
-const FromToIcons = tw.img``;
+const FromToIcons = tw.div`w-10 flex flex-col`;
+
+const Circle = tw.img`h-5 w-5`;
+
+const Line = tw.img`h-12 w-5`;
+
+const InputBoxes = tw.div``;
 
 const InputButton = tw.div`
 h-20 bg-gray-200 p-4 rounded text-2x1 flex items-center mt-8
 `;
+
