d26d38531bc423f58914f001c96907b877ce51b0
Omar Ashraf <omar.ashraf.zeinhom@Hotmail.com>
2022-06-20 01:41:52 +0200

    added-pickup-coordinates

--- pages/confirm.js
+++ pages/confirm.js
@@ -4,55 +4,50 @@ import MapComp from "../components/Map/MapComp";
 import { useEffect } from "react";
 
 const Confirm = () => {
-    //get Coordinates
+  //get Coordinates
   const getPickUpCoordiantes = () => {
     const location = "Cairo";
     // Fetch Function
     fetch(
-      `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?`+
-    new URLSearchParams({
-        access_token: "pk.eyJ1Ijoib21hcmFzaHplaW5ob20iLCJhIjoiY2w0YndzY2pjMWF1bjNjcnlqbDI1OW56YSJ9.kfQVlZWIi3vTQoPvBB8SUw",
-        limit: 1,
-    })
-    
-    
-      )
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?` +
+        new URLSearchParams({
+          access_token:
+            "pk.eyJ1Ijoib21hcmFzaHplaW5ob20iLCJhIjoiY2w0YndzY2pjMWF1bjNjcnlqbDI1OW56YSJ9.kfQVlZWIi3vTQoPvBB8SUw",
+          limit: 1,
+        })
+    )
       .then((response) => response.json())
       .then((data) => {
         console.log(data.features[0].center);
       });
   };
 
-    //get Coordinates end 
+  //get Coordinates end
 
-    // get DropOffCoordinates
+  // get DropOffCoordinates
 
-
-      //get Coordinates
+  //get Coordinates
   const getDropOffCoordiantes = () => {
     const location = "Cairo";
     // Fetch Function
     fetch(
-      `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?`+
-    new URLSearchParams({
-        access_token: "pk.eyJ1Ijoib21hcmFzaHplaW5ob20iLCJhIjoiY2w0YndzY2pjMWF1bjNjcnlqbDI1OW56YSJ9.kfQVlZWIi3vTQoPvBB8SUw",
-        limit: 1,
-    })
-    
-    
-      )
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?` +
+        new URLSearchParams({
+          access_token:
+            "pk.eyJ1Ijoib21hcmFzaHplaW5ob20iLCJhIjoiY2w0YndzY2pjMWF1bjNjcnlqbDI1OW56YSJ9.kfQVlZWIi3vTQoPvBB8SUw",
+          limit: 1,
+        })
+    )
       .then((response) => response.json())
       .then((data) => {
         console.log(data.features[0].center);
       });
   };
-
 
   //call function with useEffect
   useEffect(() => {
     getPickUpCoordiantes();
   }, []);
-
 
   return (
     <Wrapper>
