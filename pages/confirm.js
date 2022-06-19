13ef4b3f3ac8291b100f661414bb010ee2835148
Omar Ashraf <omar.ashraf.zeinhom@Hotmail.com>
2022-06-20 01:41:19 +0200

    added-pickup-coordinates

--- pages/confirm.js
+++ pages/confirm.js
@@ -4,9 +4,8 @@ import MapComp from "../components/Map/MapComp";
 import { useEffect } from "react";
 
 const Confirm = () => {
-  const getCoordiantes = () => {
-   
-
+    //get Coordinates
+  const getPickUpCoordiantes = () => {
     const location = "Cairo";
     // Fetch Function
     fetch(
@@ -20,15 +19,41 @@ const Confirm = () => {
       )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        console.log(data.features[0].center);
       });
   };
 
-  //call function with useEffect
+    //get Coordinates end 
 
+    // get DropOffCoordinates
+
+
+      //get Coordinates
+  const getDropOffCoordiantes = () => {
+    const location = "Cairo";
+    // Fetch Function
+    fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?`+
+    new URLSearchParams({
+        access_token: "pk.eyJ1Ijoib21hcmFzaHplaW5ob20iLCJhIjoiY2w0YndzY2pjMWF1bjNjcnlqbDI1OW56YSJ9.kfQVlZWIi3vTQoPvBB8SUw",
+        limit: 1,
+    })
+    
+    
+      )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.features[0].center);
+      });
+  };
+
+
+  //call function with useEffect
   useEffect(() => {
-    getCoordiantes();
+    getPickUpCoordiantes();
   }, []);
+
+
   return (
     <Wrapper>
       <MapComp />
