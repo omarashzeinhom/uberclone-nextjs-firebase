e20719870ee3ceb20ae195642dd6e37ee0e30c6e
Omar Ashraf <omar.ashraf.zeinhom@Hotmail.com>
2022-06-19 09:11:49 +0200

    added-some-styling-for-avatar-and-logo

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,8 @@ export default function Home() {
         </Header>
         {/**Action Btns */}
         <ActionButtons>
-          <ActionButton></ActionButton>
+          <ActionButton><ActionButtonImage src="https://res.cloudinary.com/dxgqvvg0z/image/upload/v1655585748/FIXITAPP/nextjs-app-images/ActionButtonImages/woker-avatar-male_mieyjc.svg"/><h5>Male Worker</h5></ActionButton>
+          <ActionButton><ActionButtonImage src="https://res.cloudinary.com/dxgqvvg0z/image/upload/v1655585697/FIXITAPP/nextjs-app-images/ActionButtonImages/worker-tool-search_au8m9f.svg"/><h5>Profession </h5></ActionButton>
         </ActionButtons>
         {/** */}
       </ActionItems>
@@ -39,7 +40,7 @@ export default function Home() {
 
 // in tail wind css you define the css3 const with tick marks not quotation
 const Wrapper = tw.div`
-flex flex-col bg-white-500 h-screen
+flex flex-col bg-white-500 h-screen text-center
 `;
 
 const ActionItems = tw.div`
@@ -71,6 +72,11 @@ rounded-full w-32 h-20 border border-gray-200 bg-stone-500/75 hover:bg-stone-600
 `;
 
 const ActionButtons = tw.div`
+flex
 `;
 
-const ActionButton = tw.div``;
+const ActionButton = tw.div`
+bg-gray-400 rounded-full flex-1 m-1 h-32 w-32 items-center`;
+
+
+const ActionButtonImage = tw.img`w-full h-3/5`;
