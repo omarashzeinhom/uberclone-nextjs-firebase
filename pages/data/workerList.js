a8d533990ab4397b0a44da45a43dc7a0651cfa04
Omar Ashraf <omar.ashraf.zeinhom@Hotmail.com>
2022-06-23 19:33:57 +0200

    added-fake-worker-list-for-training

--- pages/data/workerList.js
+++ pages/data/workerList.js
@@ -1,27 +1,34 @@
 export const workerList = [
   {
-    imgUrl: 'https://res.cloudinary.com/dxgqvvg0z/image/upload/v1656005176/FIXITAPP/nextjs-app-images/confirm/worker-svgrepo-com_1_kb1ew6.svg',
-    service: 'PremiumWorker',
+    imgUrl:
+      "https://res.cloudinary.com/dxgqvvg0z/image/upload/v1656005176/FIXITAPP/nextjs-app-images/confirm/worker-svgrepo-com_vipu1n.svg",
+    service: "Worker",
     multiplier: 1,
   },
+
   {
-    imgUrl: 'https://res.cloudinary.com/dxgqvvg0z/image/upload/v1656005176/FIXITAPP/nextjs-app-images/confirm/worker-svgrepo-com_4_rl35jn.svg',
-    service: 'Multiple Workers',
+    imgUrl:
+      "https://res.cloudinary.com/dxgqvvg0z/image/upload/v1656005555/FIXITAPP/ionic-app-images/confirm/worker-svgrepo-com_5_d2cql2.svg",
+    service: "Female Worker(Females Only)",
+    multiplier: 1,
+  },
+
+  {
+    imgUrl:
+      "https://res.cloudinary.com/dxgqvvg0z/image/upload/v1656005313/FIXITAPP/ionic-app-images/confirm/worker-svgrepo-com_1_qldnrx.svg",
+    service: "Worker+",
     multiplier: 1.5,
   },
   {
-    imgUrl: 'https://i.ibb.co/Xx4G91m/uberblack.png',
-    service: 'Black',
+    imgUrl:
+      "https://res.cloudinary.com/dxgqvvg0z/image/upload/v1656005176/FIXITAPP/nextjs-app-images/confirm/worker-svgrepo-com_3_efxhpc.svg",
+    service: "Worker Female+",
+    multiplier: 1.5,
+  },
+  {
+    imgUrl:
+      "https://res.cloudinary.com/dxgqvvg0z/image/upload/v1656005176/FIXITAPP/nextjs-app-images/confirm/worker-svgrepo-com_4_rl35jn.svg",
+    service: "Multiple Workers rate increases by 1 to be split upon workers",
     multiplier: 2,
   },
-  {
-    imgUrl: 'https://res.cloudinary.com/dxgqvvg0z/image/upload/v1656005176/FIXITAPP/nextjs-app-images/confirm/worker-svgrepo-com_vipu1n.svg',
-    service: 'Worker',
-    multiplier: 1.2,
-  },
-  {
-    imgUrl: 'https://res.cloudinary.com/dxgqvvg0z/image/upload/v1656005176/FIXITAPP/nextjs-app-images/confirm/worker-svgrepo-com_3_efxhpc.svg',
-    service: 'Female Only Workers',
-    multiplier: 2.8,
-  }
-]
+];
