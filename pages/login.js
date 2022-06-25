3dcfa5e8109d091a4e5f0293814a989f1342e968
Omar Ashraf <omar.ashraf.zeinhom@Hotmail.com>
2022-06-25 18:04:05 +0200

    added-login-route

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,10 @@
 import React from "react";
+import tw from "tailwind-styled-components/dist/tailwind";
 
 const login = () => {
-  return <div>login</div>;
+  return <Wrapper>Login</Wrapper>;
 };
 
 export default login;
+
+const Wrapper = tw.div``;
