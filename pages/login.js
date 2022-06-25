261a2acf80af48e964a840387dff796e45acc066
Omar Ashraf <omar.ashraf.zeinhom@Hotmail.com>
2022-06-25 18:26:41 +0200

    added-login-route

--- pages/login.js
+++ pages/login.js
@@ -2,9 +2,16 @@ import React from "react";
 import tw from "tailwind-styled-components/dist/tailwind";
 
 const login = () => {
-  return <Wrapper>Login</Wrapper>;
+  return (
+    <Wrapper>
+      Login
+      <SignInButton>Sign in with google</SignInButton>
+    </Wrapper>
+  );
 };
 
 export default login;
 
-const Wrapper = tw.div``;
+const Wrapper = tw.div`flex flex-col h-screen w-screen bg-gray-200`;
+
+const SignInButton = tw.button`bg-green-500 text-white text-center py-4 mt-8 selfcenter w-full `;
